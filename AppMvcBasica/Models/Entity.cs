705fb3057790c1362e4b23e9dce4b4f0f1af948f
Cleisson Vieira <cleisson.raimundi@gmail.com>
2023-11-17 15:23:31 -0300

    new project

---
+++ AppMvcBasica/Models/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace AppMvcBasica.Models
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
